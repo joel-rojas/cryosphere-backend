da74e60f37c688f6e3d6f2a0a504dc45d01582bf
Ivan Garcia <ivan.garcia@jalasoft.com>
2018-10-25 18:13:08 -0400

    Fixing author name of created file

--- src/main/java/com/cryoingdevs/common/ResourcesUtils.java
+++ src/main/java/com/cryoingdevs/common/ResourcesUtils.java
@@ -1,7 +1,7 @@
 package com.cryoingdevs.common;
 
 /**
- * Created by raul on 10/25/2018.
+ * Created by ivan on 10/25/2018.
  */
 public class ResourcesUtils {
     public static String getImagesFolderPath(){
